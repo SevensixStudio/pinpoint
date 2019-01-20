7ed1acc5b26abe7df4bf6713c5f2f36068106a1f
Summer Brochtrup <sbrochtrup@gmail.com>
2019-01-20 17:03:22 +0100

    enable logout

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,6 +10,11 @@ module.exports = (app) => {
     //google triggers this callback which in turn takes the code and sends it over to the google strategy to get the actual information for the user
     app.get('/auth/google/callback', passport.authenticate('google'));
 
+    app.get('/api/logout', (req, res) => {
+        req.logout();
+        res.send(req.user);
+    });
+
     //req is incoming request
     //res is the outgoing response
     app.get('/api/current_user', (req, res) => {
