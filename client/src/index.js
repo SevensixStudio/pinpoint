b3e548cdeda6ba86837f91867c43a224edf71e45
Summer Brochtrup <sbrochtrup@gmail.com>
2019-01-22 13:42:20 +0100

    install redux react-redux and set up store and provider

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,19 @@
+//start up redux side of things and render root component
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './components/App';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//create store
+//first argument is all of the reducers in our app
+//second argument is the initial state of the app - mostly relevant for server side rendering so not necessary for this app
+//third arg, apply middleware call
+const store = createStore(() => [], {}, applyMiddleware());
+
+    //1st arg - root component, 2nd arg - where we are attempting to render it in the dom
+ReactDOM.render(
+    <Provider store={store}><App /></Provider>, 
+    document.querySelector('#root')
+);
