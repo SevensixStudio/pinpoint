f120d3d1d6349bb13ea535937b1cda3b3902eb33
Summer Brochtrup <sbrochtrup@gmail.com>
2019-01-22 17:50:55 +0100

    return value from authReducer on fetchUser

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,6 +1,11 @@
-export default function(state = {}, action) {
-    console.log(action);
+import { FETCH_USER } from '../actions/types';
+
+export default function(state = null, action) {
     switch (action.type) {
+        case FETCH_USER:
+            //an empty string is interpretted as falsey value in JS
+            //so if action.payload is "" then the statement below will return false
+            return action.payload || false;
         default: 
             return state;
     }
