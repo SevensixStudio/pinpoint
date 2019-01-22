d79b938c60d7b997a23d71baab53a01988effd84
Summer Brochtrup <sbrochtrup@gmail.com>
2019-01-22 21:38:23 +0100

    wire up login/logout links

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,23 +1,44 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
+    renderContent() {
+       switch (this.props.auth) {
+           case null:
+                return;
+           case false:
+                return (
+                    <ul className="right">
+                        <li><a href="/auth/google">Login with Google</a></li>
+                        <li><a href="/auth/facebook">Login with Facebook</a></li>
+                    </ul>
+                );
+           default: 
+                return (
+                    <ul className="right">
+                        <li><a href="/api/logout">Logout</a></li>
+                    </ul>
+                );
+       }
+    }
+
     render() {
         return (
             <nav>
-                <div class="nav-wrapper">
-                    <a class="left brand-logo">PinPoint</a>
-                    <ul class="right">
-                        <li>
-                            <a>Login with Google</a>
-                        </li>
-                        <li>
-                            <a>Login with Facebook</a>
-                        </li>
-                    </ul>
+                <div className="nav-wrapper">
+                    <Link 
+                        to={this.props.auth ? '/surveys' : '/' } 
+                        className="left brand-logo">PinPoint</Link>
+                    {this.renderContent()}
                 </div>
             </nav>
         );
     }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+    return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
