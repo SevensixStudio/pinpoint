f5f9b1b76facf6639ff48aeedddb9b1fb8853a6c
Summer Brochtrup <sbrochtrup@gmail.com>
2019-01-22 14:18:01 +0100

    setup react router with placeholder components for each route

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+//BrowserRouter is the brains of react-router -- it tells react router which components to show depending on the current url
 
+const Header = () => <h2>Header</h2>
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveyNew = () => <h2>SurveyNew</h2>
+const Landing = () => <h2>Landing</h2>
+
+//BrowserRouter can have at most ONE child
 const App = () => {
     return (
         <div>
-            Hi there!
+            <BrowserRouter>
+                <div>
+                    <Header />
+                    <Route exact path="/" component={Landing} />
+                    <Route exact path="/surveys" component={Dashboard} />
+                    <Route path="/surveys/new" component={SurveyNew} />
+                </div>
+            </BrowserRouter>
         </div>
     )
 };
