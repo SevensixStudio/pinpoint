a7115cd8b4a0b0ab683bb41102ffe84ede7a59fd
Summer Brochtrup <sbrochtrup@gmail.com>
2019-01-22 16:49:39 +0100

    install and setup materialize.css, redux thunk, and axios

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,10 @@
 //start up redux side of things and render root component
+import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
@@ -11,7 +13,7 @@ import reducers from './reducers';
 //first argument is all of the reducers in our app
 //second argument is the initial state of the app - mostly relevant for server side rendering so not necessary for this app
 //third arg, apply middleware call
-const store = createStore(reducers, {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
     //1st arg - root component, 2nd arg - where we are attempting to render it in the dom
 ReactDOM.render(
