10fb4ad315188ef3ed6195939e9ec5dc55e6f174
Summer Brochtrup <sbrochtrup@gmail.com>
2019-01-28 19:03:17 +0100

    add basic survey list styles

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -2,20 +2,33 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchSurveys } from '../../actions';
 
+import '../../index.scss';
+import './SurveyList.scss';
+
 class SurveyList extends Component {
     componentDidMount() {
+        console.log(this.props.auth);
         if (this.props.auth) {
+        
             this.props.fetchSurveys();
         }
     }
 
     renderSurveys() {
+        console.log(this.props.surveys);
+        if (this.props.surveys.length === 0) {
+            return <p>NO SURVEYS</p>;
+        }
         return this.props.surveys.reverse().map(survey => {
             return (
-                <div key={survey._id}>
+                <div key={survey._id} className="SurveyList__item">
                     <div>
                         <span>{survey.title}</span>
+                        <p>Edit: show on hover</p>
+                        <p>Send: show on hover</p>
+                        <p>Delete: show on hover</p>
                         <p>{survey.body}</p>
+                        <p>Created on: 1/1/1111</p>
                         <p>
                             Sent on: {new Date(survey.dateSent).toLocaleDateString()}
                         </p>
@@ -23,6 +36,7 @@ class SurveyList extends Component {
                     <div>
                         <a>Yes: {survey.yes}</a>
                         <a>No: {survey.no}</a>
+                        <p>Pretty circle diagram</p>
                     </div>
                 </div>
             );
@@ -31,7 +45,7 @@ class SurveyList extends Component {
     
     render() {
         return (
-            <div>
+            <div className="SurveyList">
                 {this.renderSurveys()}
             </div>
         );
