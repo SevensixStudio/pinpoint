7d6c1186f8035d980de548f62c80b68607b102a3
Summer Brochtrup <sbrochtrup@gmail.com>
2019-01-30 14:23:41 +0100

    change class to className

--- client/src/components/steps/StepDots.js
+++ client/src/components/steps/StepDots.js
@@ -8,20 +8,20 @@ const StepDots = ({ numberOfSteps, currentStep }) => {
     function renderSteps() {
         const rows = [];
         for (let i = 1; i <= numberOfSteps; i++) {
-            const className = "StepDots__step";
-            if (i == currentStep) {
+            let className = "StepDots__step";
+            if (i === currentStep) {
                 className += " StepDots__step--current";
             } else if (i < currentStep) {
                 className += " StepDots__step--stepFinished";
             }
-            rows.push(<span class={className} key={i}></span>);
+            rows.push(<span className={className} key={i}></span>);
         }
         return rows;
     }
 
     
     return (
-        <div class="StepDots">
+        <div className="StepDots">
             {renderSteps()}
         </div>
     );
