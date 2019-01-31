aa7544bc654e54be57db412f482cad1efec7ddb4
Summer Brochtrup <sbrochtrup@gmail.com>
2019-01-31 08:08:41 +0100

    redirect to /dashboard on sumitSurvey

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -17,7 +17,7 @@ export const handleToken = (token, cost, credits) => async dispatch => {
 
 export const submitSurvey = (values, history) => async dispatch => {
     const res = await axios.post('/api/surveys', values);
-    history.push('/surveys');
+    history.push('/dashboard');
     dispatch({ type: FETCH_USER, payload: res.data});
 };
 
