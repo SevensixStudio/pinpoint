9090f2180901c345ae512cd525bd3445ba7ee1bc
Summer Brochtrup <sbrochtrup@gmail.com>
2019-02-02 13:44:58 +0100

    conditionally render toolbar tools, reformat last responded date

--- client/src/components/surveyList/SurveyList.js
+++ client/src/components/surveyList/SurveyList.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { fetchSurveys, deleteSurvey } from '../../actions';
+import TimeAgo from 'timeago-react';
 import CircleGraph from '../circleGraph/CircleGraph';
 
 import '../../index.scss';
@@ -19,13 +21,15 @@ class SurveyList extends Component {
             return <p className="SurveyList__empty">You haven't created any surveys yet. <a className="inline-link" href="/surveys/new">Create one now!</a></p>;
         }
         return this.props.surveys.map(survey => {
-            return (
+            return (  
                 <div key={survey._id} className="SurveyList__item">
                     <div className="SurveyList__item--titleBox">
-                        <h3>{survey.surveyName}</h3>
+                        <Link to={`/surveys/preview/${survey._id}`} className="removeLinkStyles">
+                            <h3>{survey.surveyName}</h3>
+                        </Link>
                         <p className="subject">Subject: {survey.subject}</p>
+                        <p className="date">{survey.dateSent ? ('Sent on: ' + new Date(survey.dateSent).toLocaleDateString() + " " + new Date(survey.dateSent).toLocaleTimeString()) : "This survey has not been sent yet"}</p>
                         <p className="date">Created on: {new Date(survey.dateCreated).toLocaleDateString() + " " + new Date(survey.dateCreated).toLocaleTimeString()}</p>
-                        <p className="date">Sent on: {survey.dateSent ? (new Date(survey.dateSent).toLocaleDateString() + " " + new Date(survey.dateSent).toLocaleTimeString()) : "This survey has not been sent yet"}</p>
                     </div>
                     <YesNoStats className="SurveyList__item--numbers" yesCount={survey.yes} noCount={survey.no} total={survey.totalRecipients} />
                     <div className="SurveyList__item--graph">
@@ -33,18 +37,19 @@ class SurveyList extends Component {
                     </div>
                     <div className="SurveyList__item--lastResponse">
                         <hr />
-                        <p className="date">Last response recieved: {survey.lastResponded ? (new Date(survey.lastResponded).toLocaleDateString() + " at " + new Date(survey.lastResponded).toLocaleTimeString()) : 'No one has responded to your survey yet'}</p>
+                        {survey.lastResponded ? [<p className="date">Last response recieved: </p>,<TimeAgo className="date--ago" datetime={new Date(survey.lastResponded)} />] : <p className="date">No one has responded to your survey yet</p>}
                     </div>
                     <div key={survey._id} className="SurveyList__item--toolsPanel" id="tools">
                         <div className="SurveyList__item--toolsPanel--action">
                             <a href={`/surveys/preview/${survey._id}`}><i className="far fa-eye"></i></a>
                         </div>
-                        <div className="SurveyList__item--toolsPanel--action">
-                            <a href="#"><i className="far fa-envelope"></i></a>
-                        </div>
-                        <div className="SurveyList__item--toolsPanel--action">
-                            <a href="#"><i className="far fa-edit"></i></a>
-                        </div>
+                        {survey.isDraft && [
+                            <div className="SurveyList__item--toolsPanel--action">
+                                <a href="#"><i className="far fa-envelope"></i></a>
+                            </div>,
+                            <div className="SurveyList__item--toolsPanel--action">
+                                <a href="#"><i className="far fa-edit"></i></a>
+                            </div>]}
                         <div className="SurveyList__item--toolsPanel--action" onClick={() => this.props.deleteSurvey(survey._id)}>
                             <a href="#"><i className="far fa-trash-alt"></i></a>
                         </div>
@@ -63,8 +68,8 @@ class SurveyList extends Component {
     }
 }
 
-function mapStateToProps({ surveys, auth }) {
-    return { surveys, auth };
+function mapStateToProps({ surveys, auth, state }) {
+    return { surveys, auth, state };
 }
 
 export default connect(mapStateToProps, { fetchSurveys, deleteSurvey })(SurveyList);
