0e6cc0d75aab1763513b2d280ab2fa1af54b4f6e
Summer Brochtrup <sbrochtrup@gmail.com>
2019-02-03 11:14:47 +0100

    remove un-used 'onFailure' call and pull out error message to end to onRequestError

--- client/src/middleware/asyncMiddleware.js
+++ client/src/middleware/asyncMiddleware.js
@@ -1,7 +1,5 @@
 import axios from "axios";
-
 import { ASYNC_REQUEST } from "../actions/types";
-
 import { accessDenied, onRequestError, requestStart, requestEnd } from "../actions/index";
 
 
@@ -15,17 +13,15 @@ const asyncMiddleware = ({ dispatch }) => next => action => {
         method,
         data,
         onSuccess,
-        onFailure,
         label
     } = action.payload;
-
-    console.log(label);
 
     const dataOrParams = ["GET", "DELETE"].includes(method) ? "params" : "data";
 
     if (label) {
         dispatch(requestStart(label));
     }
+
 
   axios
     .request({
@@ -37,9 +33,8 @@ const asyncMiddleware = ({ dispatch }) => next => action => {
       dispatch(onSuccess(data));
     })
     .catch(error => {
-      console.log(error);
-      dispatch(onRequestError(label, error));
-      dispatch(onFailure(error)); //maybe delete this?
+      console.log(error.response.data.error);
+      dispatch(onRequestError(label, error.response.data.error));
 
       if (error.response && error.response.status === 403) {
         dispatch(accessDenied(window.location.pathname));
